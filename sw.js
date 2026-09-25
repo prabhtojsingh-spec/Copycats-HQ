0cd3c785308cd96aed54834c8da74c857177aa29
prabhtojsingh-spec <prabhtojsingh@gmail.com>
2026-09-25 05:48:41 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE='copycats-hq-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./apple-touch-icon.png','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>{
+  const u=new URL(e.request.url);
+  if(u.origin===location.origin){
+    e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)));
+  }
+});
