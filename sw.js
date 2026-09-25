c96604842db035d5659c3e3cc932e96a9e3aae59
prabhtojsingh <prabhtojsingh@gmail.com>
2026-09-25 19:09:52 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='copycats-hq-v4-safearea';
+const CACHE='copycats-hq-v5-showfix';
 const SHELL=['./','./index.html','./manifest.webmanifest','./apple-touch-icon.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
